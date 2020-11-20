1973c1a72397060c1a7d6d11e7d49f10ad9885da
Elliott Maguire <me@elliott-m.com>
2020-11-20 15:06:06 -0800

    add example stock parser code

--- src/cscd350f20project/ParserCreate/Stock.java
+++ src/cscd350f20project/ParserCreate/Stock.java
@@ -1,4 +1,30 @@
 package cscd350f20project.ParserCreate;
 
+import cs350f20project.controller.cli.parser.MyParserHelper;
+import cs350f20project.controller.command.A_Command;
+import cs350f20project.controller.command.creational.CommandCreateStockCarBox;
+import cs350f20project.controller.command.creational.CommandCreateStockCarCaboose;
+
+import java.lang.reflect.Array;
+
 public class Stock {
+    private MyParserHelper myParserHelper;
+
+    public Stock(MyParserHelper myParserHelper) {
+        this.myParserHelper = myParserHelper;
+    }
+
+    public void ParseCreate(String command) {
+        String[] words = command.split(" ");
+        String id = words[3];
+        String type = words[5];
+
+        A_Command c;
+        switch (type.toLowerCase()) {
+            case "box" -> {
+                c = new CommandCreateStockCarBox(id);
+                myParserHelper.getActionProcessor().schedule(c);
+            }
+        }
+    }
 }
