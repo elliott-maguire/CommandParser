2da6ab89bf711a09ecd5b1fe94a32d60e13081e7
Elliott Maguire <me@elliott-m.com>
2020-11-21 16:07:42 -0800

    add ParserSysEnv class base

--- src/cscd350f20project/ParserSysEnv.java
+++ src/cscd350f20project/ParserSysEnv.java
@@ -1,4 +1,22 @@
 package cscd350f20project;
 
+import cs350f20project.controller.cli.parser.MyParserHelper;
+import cs350f20project.controller.command.A_Command;
+import cs350f20project.controller.command.meta.CommandMetaDoExit;
+import cs350f20project.controller.command.meta.CommandMetaDoRun;
+import cs350f20project.controller.command.meta.CommandMetaViewDestroy;
+import cs350f20project.controller.command.structural.CommandStructuralCommit;
+import cs350f20project.controller.command.structural.CommandStructuralCouple;
+import cs350f20project.controller.command.structural.CommandStructuralUncouple;
+
+import javax.swing.text.html.parser.Parser;
+
 public class ParserSysEnv {
+    private MyParserHelper myParserHelper;
+
+    public ParserSysEnv(MyParserHelper myParserHelper) {
+        this.myParserHelper = myParserHelper;
+    }
+
+
 }
