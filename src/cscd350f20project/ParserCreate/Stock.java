a4c96e01e76aeca387829a3b8c7b7af1d434c5bc
Julian Welge <julianwelge@gmail.com>
2020-11-28 16:35:28 -0800

    all commands for stock except 34

--- src/cscd350f20project/ParserCreate/Stock.java
+++ src/cscd350f20project/ParserCreate/Stock.java
@@ -4,6 +4,10 @@ import cs350f20project.controller.cli.parser.MyParserHelper;
 import cs350f20project.controller.command.A_Command;
 import cs350f20project.controller.command.creational.CommandCreateStockCarBox;
 import cs350f20project.controller.command.creational.CommandCreateStockCarCaboose;
+import cs350f20project.controller.command.creational.CommandCreateStockCarFlatbed;
+import cs350f20project.controller.command.creational.CommandCreateStockCarPassenger;
+import cs350f20project.controller.command.creational.CommandCreateStockCarTank;
+import cs350f20project.controller.command.creational.CommandCreateStockCarTender;
 
 import java.lang.reflect.Array;
 
@@ -25,6 +29,32 @@ public class Stock {
                 c = new CommandCreateStockCarBox(id);
                 myParserHelper.getActionProcessor().schedule(c);
             }
+
+            case "caboose" -> {
+                c = new CommandCreateStockCarCaboose(id);
+                myParserHelper.getActionProcessor().schedule(c);
+            }
+
+            case "flatbed" -> {
+                c = new CommandCreateStockCarFlatbed(id);
+                myParserHelper.getActionProcessor().schedule(c);
+            }
+
+            case "passenger" -> {
+                c = new CommandCreateStockCarPassenger(id);
+                myParserHelper.getActionProcessor().schedule(c);
+            }
+
+            case "tank" -> {
+                c = new CommandCreateStockCarTank(id);
+                myParserHelper.getActionProcessor().schedule(c);
+            }
+
+            case "tender" -> {
+                c = new CommandCreateStockCarTender(id);
+                myParserHelper.getActionProcessor().schedule(c);
+            }
+            
         }
     }
 }
